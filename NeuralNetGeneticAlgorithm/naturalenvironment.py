f28c661d77335453b796ee83178ef775dabc4bb6
Clayton Jones <clayrock78@gmail.com>
2023-05-04 14:02:09 -0400

    suicide

--- NeuralNetGeneticAlgorithm/naturalenvironment.py
+++ NeuralNetGeneticAlgorithm/naturalenvironment.py
@@ -12,16 +12,17 @@ PLAYERWIDTH = 10
 class Player():
     def __init__(self):
 
-        self.x = random.random() * WIDTH
-        self.y = random.random() * HEIGHT
+        self.x = random.randint(0,WIDTH  * 1000)
+        self.y = random.randint(0,HEIGHT * 1000)
+
         self.pos = pygame.Rect(self.x, self.y, PLAYERWIDTH, PLAYERWIDTH)
         
 
     def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
-        self.pos.centerx += dx
-        self.pos.centery += dy
+        self.x += dx*300
+        self.y += dy*300
+        self.pos.centerx = round(self.x/1000)
+        self.pos.centery = round(self.y/1000)
         
         
 
@@ -45,9 +46,9 @@ def find_fitness(population):
     food_pool = [[random.random() * WIDTH, random.random() * HEIGHT] for i in range(1)]
 
     player_pool = [Player() for x in range(pop_size)]
-    fitness_values = [0 for x in player_pool]
+    fitness_values = [0 for _ in player_pool]
     
-    for tick in range(200):
+    for tick in range(1000):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -59,39 +60,33 @@ def find_fitness(population):
                 pygame.draw.circle(surface, lime_green, [food[0], food[1]], FOODRADIUS)
 
         for pop_index, player in enumerate(player_pool):
-            colour = (100,0,0)
-            for food_index, food in enumerate(food_pool):
-                #if (food[0] - player.x) ** 2 +  (food[1] - player.y) ** 2 < 40:
-                
-                dist = math.sqrt((player.pos.centerx - food[0]) ** 2 + (player.pos.centery - food[1]) ** 2)
-                #fitness_values[pop_index] = 1/ (dist / WIDTH)
-                if dist < FOODRADIUS + PLAYERWIDTH:#player.pos.collidepoint(food):
-                    colour = (0,0,200)
-                    fitness_values[pop_index] += 10
-                    #food_pool[food_index] = (random.random() * WIDTH, random.random() * HEIGHT)
 
             # check bounds
             if player.pos.centerx > WIDTH or player.pos.centerx < 0 or player.pos.centery > HEIGHT or player.pos.centery < 0:
-                fitness_values[pop_index] -= 0
-            else:
-                fitness_values[pop_index] += 0
-                input_layer = food_pool
+                pass
+                #fitness_values[pop_index] -= 0
+        
+            #fitness_values[pop_index] += 0
+            """
+            input_layer = food_pool
+            input_layer = list(np.array(input_layer).flatten())
+            input_layer.append(player.x)
+            input_layer.append(player.y)
+            input_layer = [x/WIDTH for x in input_layer]
+            """
+            input_layer = [player.pos.centerx/WIDTH, player.pos.centery/HEIGHT, food_pool[0][0]/WIDTH, food_pool[0][1]/HEIGHT]
+            velocity = population[pop_index].activate(input_layer)
 
-                input_layer = list(np.array(input_layer).flatten())
-                input_layer.append(player.x)
-                input_layer.append(player.y)
-                input_layer = [x/WIDTH for x in input_layer]
-
-                velocity = population[pop_index].activate(input_layer)
-                if sum(velocity) == 0:
-                    fitness_values[pop_index] -= 100
-
-                player.move(velocity[0] * dt - velocity[1] * dt, velocity[2] * dt - velocity[3] * dt)
-                if (DISPLAY_STUF):
-                    if pop_index == pop_size-1:
-                        player.display()
-                    else:
-                        player.display(colour)
+            #player.move(velocity[0] * dt - velocity[1] * dt, velocity[2] * dt - velocity[3] * dt)
+            dx = velocity[0] * dt - velocity[1] * dt
+            dy = velocity[2] * dt - velocity[3] * dt
+            #print(dx, dy)
+            player.move(dx, dy)
+            if (DISPLAY_STUF):
+                player.display()
+        
+            dist = ((player.pos.centerx - food[0]) ** 2 + (player.pos.centery - food[1]) ** 2)**.5
+            fitness_values[pop_index] = WIDTH - dist
 
 
         if (DISPLAY_STUF):
